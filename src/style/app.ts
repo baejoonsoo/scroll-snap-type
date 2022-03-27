98b69470be35285973db6e658800e89f0fcf3965
baejoonsoo <artns25@dsm.hs.kr>
2022-03-27 16:00:44 +0900

    init

---
+++ src/style/app.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Page = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
