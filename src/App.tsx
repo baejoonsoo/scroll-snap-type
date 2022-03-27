fa555f88bc1ef345f3f92432ec76fd1b97a5b9d1
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-27 16:48:41 +0900

    image scroll box

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import * as S from "./style/app";
+const image = importAll(require.context("../public/img"));
+
+function importAll(r: any) {
+  let img: string[] = [];
+  r.keys().forEach((item: string, index: number) => {
+    img[index] = item;
+  });
+  return img;
+}
 
 function App() {
   return (
     <S.Page>
-      <div>aa</div>
+      <S.ScrollBox>
+        {image.map((img, index) => (
+          <S.ItemBox key={index} img={"img/" + img} />
+        ))}
+      </S.ScrollBox>
     </S.Page>
   );
 }
--- src/style/app.ts
+++ src/style/app.ts
@@ -7,3 +7,21 @@ export const Page = styled.main`
   justify-content: center;
   align-items: center;
 `;
+
+export const ScrollBox = styled.section`
+  width: 300px;
+  height: 300px;
+
+  overflow-y: auto;
+`;
+
+export const ItemBox = styled.div`
+  width: 300px;
+  height: 100%;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  background-image: url(${({ img }: { img: string }) => img});
+`;
